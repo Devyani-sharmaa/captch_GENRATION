6819873af7153cdddadfc320276858cb82220871
devyani <devyanibrd123@gmail.com>
2024-09-03 16:36:03 +0530

    random genration

---
+++ hi.py
@@ -0,0 +1,10 @@
+from captcha.image import ImageCaptcha
+
+image_info = ImageCaptcha(width=250,height=100)
+
+
+
+captcha_text="devyani"
+source=image_info.generate(captcha_text)
+
+image_info.write(captcha_text,'Captcha.png')
